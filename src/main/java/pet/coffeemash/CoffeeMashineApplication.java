f1da17b1452cad659b768bb000d90d704bea7445
Yakupov Aziz <asisdes@gmail.com>
2025-01-04 19:57:32 +0500

    Init commit

---
+++ src/main/java/pet/coffeemash/CoffeeMashineApplication.java
@@ -0,0 +1,13 @@
+package pet.coffeemash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMashineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMashineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/coffeemash/CoffeeMashineApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.coffeemash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMashineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
